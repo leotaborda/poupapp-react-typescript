1c7d590acbb8314ebdec42aefb076cc011a4b144
leotaborda <leo.taboorda@gmail.com>
2025-08-22 11:12:29 -0300

    feat: dealing with state global transaction

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,22 +1,33 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { IUsuario } from "../types";
-import { obterUsuario, criarUsuario } from "../api";
+import { ITransacoes, IUsuario } from "../types";
+import {
+  obterUsuario,
+  criarUsuario,
+  obterTransacoes,
+  criarTransacao,
+} from "../api";
 
 interface AppContextType {
   usuario: IUsuario | null;
   criaUsuario: (usuario: Omit<IUsuario, "id">) => Promise<void>;
+  transacoes: ITransacoes[];
+  criaTransacao: (transacao: Omit<ITransacoes, "id">) => Promise<void>;
 }
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [usuario, setUsuario] = useState<IUsuario | null>(null);
+  const [transacoes, setTransacoes] = useState<ITransacoes[]>([]);
 
   const carregaDadosUsuario = async () => {
     try {
       const usuarios = await obterUsuario();
+      const transacoes = await obterTransacoes();
       if (usuarios.length > 0) {
         setUsuario(usuarios[0]);
+        setTransacoes(transacoes);
       }
     } catch (err) {
       console.log(err);
@@ -36,8 +47,19 @@ const AppProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  const criaTransacao = async (novaTransacao: Omit<ITransacoes, "id">) => {
+    try {
+      const transacaoCriada = await criarTransacao(novaTransacao);
+      setTransacoes((prev) => [...prev, transacaoCriada]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <AppContext.Provider value={{ usuario, criaUsuario }}>
+    <AppContext.Provider
+      value={{ usuario, criaUsuario, transacoes, criaTransacao }}
+    >
       {children}
     </AppContext.Provider>
   );
